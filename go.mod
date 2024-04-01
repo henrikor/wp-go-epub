b3f6f9b7de9e8ff20073bf718fc7cb78af1adbab
Henrik Ormåsen <henrik@Emil-Vallersnes-MacBook-Pro.local>
2024-04-02 00:07:03 +0200

    Treffer ikke alle seksjoner enda

--- go.mod
+++ go.mod
@@ -5,8 +5,12 @@ go 1.22.1
 require github.com/go-shiori/go-epub v1.2.1
 
 require (
+	github.com/fatih/color v1.16.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
 	github.com/gofrs/uuid/v5 v5.0.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/vincent-petithory/dataurl v1.0.0 // indirect
 	golang.org/x/net v0.22.0 // indirect
+	golang.org/x/sys v0.18.0 // indirect
 )
--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"regexp"
 	"strings"
 
+	"github.com/fatih/color"
 	"github.com/go-shiori/go-epub"
 )
 
@@ -34,8 +35,6 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	// Set the author
 	e.SetAuthor(*author)
 	e.SetTitle(*title)
 
@@ -48,20 +47,45 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	ncontent := strings.Replace(string(content), "&nbsp;", " ", -1)
 	ncontent = strings.ReplaceAll(ncontent, "<!-- wp:heading -->", "<!-- post2epub:section-end -->\n<!-- post2epub:section-start -->\n<!-- wp:heading -->")
 	ncontent = strings.Replace(ncontent, "<!-- post2epub:section-end -->", "", 1)
 
 	re := regexp.MustCompile(`(?s)<!-- post2epub:section-start -->(.*?)<!-- post2epub:section-end -->`)
 	matches := re.FindAllStringSubmatch(ncontent, -1)
-	i := 0
-	for _, match := range matches {
-		heading := fmt.Sprintf("test %v", i)
-		_, err := e.AddSection(match[i], heading, "", "")
+
+	reh2 := regexp.MustCompile(`<h2.*?>(.*?)<\/h2>`)
+	reh3 := regexp.MustCompile(`<h3.*?>(.*?)<\/h3>`)
+	for i, match := range matches {
+		if i == len(matches)-1 {
+			break
+		}
+		color.Yellow("Prints Index: %d", i)
+		//color.White("Prints match %v", match)
+		section := string(match[i])
+		tmp := reh2.FindStringSubmatch(section)
+		heading := tmp[1]
+		filenameSection := fmt.Sprintf("section%05d", i)
+		_, err := e.AddSection(match[i], heading, filenameSection, "")
 		if err != nil {
 			log.Fatal(err)
 		}
+		// Subsection:
+
+		reSub := regexp.MustCompile(`(?s)<!-- post2epub:section-sub-start -->(.*?)<!-- post2epub:section-sub-end -->`)
+		matchesSub := reSub.FindAllStringSubmatch(section, -1)
+		for x, matchSub := range matchesSub {
+			section := string(matchSub[i])
+			tmp := reh3.FindStringSubmatch(section)
+			heading := tmp[1]
+			filenameSubSection := fmt.Sprintf("%s-sub-%05d", filenameSection, x)
+
+			_, err := e.AddSubSection(filenameSection, matchSub[i], heading, filenameSubSection, "")
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+
 	}
 
 	// Write the EPUB
