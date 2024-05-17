627f2010a5ece1bcae2eb041d7df2c773123dbd4
Henrik Ormåsen <henrik.ormasen@gmail.com>
2024-05-17 17:33:46 +0200

    siste før swithch tilbake til regexp

--- go.mod
+++ go.mod
@@ -2,13 +2,16 @@ module henrikor/wp-go-epub
 
 go 1.22.1
 
-require github.com/go-shiori/go-epub v1.2.1
+require (
+	github.com/PuerkitoBio/goquery v1.9.2
+	github.com/djimenez/iconv-go v0.0.0-20160305225143-8960e66bd3da
+)
 
 require (
-	github.com/PuerkitoBio/goquery v1.9.2 // indirect
 	github.com/andybalholm/cascadia v1.3.2 // indirect
 	github.com/fatih/color v1.16.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
+	github.com/go-shiori/go-epub v1.2.1 // indirect
 	github.com/gofrs/uuid/v5 v5.0.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
--- main.go
+++ main.go
@@ -16,7 +16,13 @@ func main() {
 		return
 	}
 	defer file.Close()
-
+	// Convert the designated charset HTML to utf-8 encoded HTML.
+	// `charset` being one of the charsets known by the iconv package.
+	// utfBody, err := iconv.NewReader(file, charset, "utf-8")
+	// if err != nil {
+	// 	// handler error
+	// }
+	// doc, err := goquery.NewDocumentFromReader(utfBody)
 	doc, err := goquery.NewDocumentFromReader(file)
 	if err != nil {
 		fmt.Println("Error parsing HTML:", err)
@@ -24,17 +30,34 @@ func main() {
 	}
 
 	doc.Find("h2").Each(func(i int, s *goquery.Selection) {
-		sectionTitle := s.Text()
-		fmt.Println("-------------- SECTION ----------------- \n" + sectionTitle)
+		// sectionTitle, err := s.Html()
+		// if err != nil {
+		// 	color.RedString("Err: %v", err)
+		// }
+		title := s.Closest("h2")
 
-		s.NextUntil("h2").Each(func(j int, s *goquery.Selection) {
-			subsectionTitle := s.Text()
-			fmt.Println("-------------- SUBSECTION 1 ----------------\n" + subsectionTitle)
-			// s.NextUntil("h2, h3").Each(func(k int, s *goquery.Selection) {
-			// 	title := s.Text()
-			// 	fmt.Println("---------------- SUBSECTION 2 / 3 -----------------\n" + title)
-			// })
+		fmt.Println("-------------- title2 ----------------- \n" + title.Text())
+		// fmt.Println("-------------- SECTION ----------------- \n" + sectionTitle)
+
+		s.NextUntil("h2").Filter("h3").Each(func(j int, s *goquery.Selection) {
+			// Print out the text of the h2 element
+			title := s.Closest("h3")
+			fmt.Println("-------------- title3 ----------------- \n" + title.Text())
+
+			fmt.Println("h3 element text:", s.Prev().Text())
+
+			// Now you can process h3 elements
 		})
+
+		// s.NextUntil("h2").Each(func(j int, s *goquery.Selection) {
+		// 	sub1 := s.Text()
+		// 	fmt.Println("-------------- SUBSECTION 1 ----------------\n" + sub1)
+
+		// 	// s.NextUntil("h2, h3").Each(func(k int, s *goquery.Selection) {
+		// 	// 	title := s.Text()
+		// 	// 	fmt.Println("---------------- SUBSECTION 2 / 3 -----------------\n" + title)
+		// 	// })
+		// })
 	})
 
 }
